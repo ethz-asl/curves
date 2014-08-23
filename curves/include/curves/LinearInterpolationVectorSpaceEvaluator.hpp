a9da64afa7df5d262dcc877d717875b1ea77882f
Renaud Dube <renaud.dube@mavt.ethz.ch>
2014-08-23 09:20:45 +0200

    initial implementation of an instanciable LinearInterpolationVectorSpaceEvaluator object

--- curves/include/curves/LinearInterpolationVectorSpaceEvaluator.hpp
+++ curves/include/curves/LinearInterpolationVectorSpaceEvaluator.hpp
@@ -1,34 +1,53 @@
 #ifndef CURVES_LINEAR_INTERPOLATION_VECTOR_SPACE_EVALUATOR_HPP
 #define CURVES_LINEAR_INTERPOLATION_VECTOR_SPACE_EVALUATOR_HPP
 
+#include "Evaluator.hpp"
+#include "VectorSpaceConfig.hpp"
+
 namespace curves {
 
-class LinearInterpolationVectorSpaceEvaluator {
- public:
-  LinearInterpolationVectorSpaceEvaluator();
-  virtual ~LinearInterpolationVectorSpaceEvaluator();
+class LinearInterpolationVectorSpaceEvaluator : public Evaluator<VectorSpaceConfig> {
 
-  virtual void getKeys(std::vector<Key>& outKeys);
-  
-  virtual void getCoefficients(std::vector<Key>& outCoefficients);
+ public:
+  typedef Evaluator<VectorSpaceConfig> Parent;
+  typedef Parent::DerivativeType DerivativeType;
+  typedef Parent::ValueType ValueType;
+
+  LinearInterpolationVectorSpaceEvaluator() {}
+  virtual ~LinearInterpolationVectorSpaceEvaluator() {}
+
+  virtual void getKeys(std::vector<Key> *outKeys) const;
+
+  virtual void getCoefficients(std::vector<Coefficient>* outCoefficients) const;
+
+  /// Evaluate the ambient space of the curve (functional form).
+  virtual ValueType evaluate(const std::vector<Coefficient>& coefficients) const;
+
+  /// Evaluate the ambient space of the curve (functional form).
+  virtual ValueType evaluateAndJacobian(const std::vector<Coefficient>& coefficients,
+                                        std::vector<Eigen::MatrixXd>* outJacobian) const;
 
   /// Evaluate the ambient space of the curve.
-  virtual Eigen::VectorXd evaluateVector();
-  
+  virtual Eigen::VectorXd evaluateVector() const;
+
   /// Evaluate the curve derivatives.
-  virtual Eigen::VectorXd evaluateDerivative(unsigned derivativeOrder);
+  virtual Eigen::VectorXd evaluateDerivative(unsigned derivativeOrder) const;
 
   /// Evaluate the ambient space of the curve (functional form).
-  virtual Eigen::VectorXd evaluateVector(const std::vector<Coefficient>& coefficients);
-  
+  virtual Eigen::VectorXd evaluateVector(const std::vector<Coefficient>& coefficients) const;
+
   /// Evaluate the curve derivatives (functional form).
-  virtual Eigen::VectorXd evaluateDerivative(unsigned derivativeOrder, const std::vector<Coefficient>& coefficients);
+  virtual Eigen::VectorXd evaluateDerivative(unsigned derivativeOrder,
+                                             const std::vector<Coefficient>& coefficients) const;
 
   /// Evaluate the ambient space of the curve (functional form).
-  virtual Eigen::VectorXd evaluateVectorAndJacobian(const std::vector<Coefficient>& coefficients, std::vector<Eigen::MatrixXd>* outJacobian);
-  
+  virtual Eigen::VectorXd evaluateVectorAndJacobian(const std::vector<Coefficient>& coefficients,
+                                                    std::vector<Eigen::MatrixXd>* outJacobian) const;
+
   /// Evaluate the curve derivatives (functional form).
-  virtual Eigen::VectorXd evaluateDerivativeAndJacobian(unsigned derivativeOrder, const std::vector<Coefficient>& coefficients, std::vector<Eigen::MatrixXd>* outJacobian);
+  virtual Eigen::VectorXd evaluateDerivativeAndJacobian(unsigned derivativeOrder,
+                                                        const std::vector<Coefficient>& coefficients,
+                                                        std::vector<Eigen::MatrixXd>* outJacobian) const;
 
 };
 
