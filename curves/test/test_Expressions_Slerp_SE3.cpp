203f421da12c37aa8a3a4d8480e5f44cfc0991db
gawela <abel.gawel@mavt.ethz.ch>
2014-11-07 17:26:07 +0100

    added test for relative measurements and dynamic handling of keys in ExpressionFactor

--- curves/test/test_Expressions_Slerp_SE3.cpp
+++ curves/test/test_Expressions_Slerp_SE3.cpp
@@ -19,6 +19,7 @@ typedef typename SlerpSE3Curve::ValueType ValueType;
 typedef kindr::minimal::QuatTransformationTemplate<double> SE3;
 typedef SE3::Rotation SO3;
 
+// definition of traits for SE3 (to act as gtsam values)
 namespace gtsam {
 namespace traits {
 
@@ -38,7 +39,7 @@ struct print<SE3> {
   typedef void result_type;
   void operator()(const SE3& obj, const std::string& str) {
     // make nicer layout
-    std:: cout << "SE3 object: " << std::endl;
+    std:: cout << str << std::endl;
     std:: cout << "Position: " << obj.getPosition() << std::endl;
     std:: cout << "Rotation: "<< obj.getRotation() << std::endl;
   }
@@ -46,6 +47,7 @@ struct print<SE3> {
 } // namespace traits
 } // namespace gtsam
 
+// definition of charts for SE3, to be used in gtsam framework
 namespace gtsam {
 typedef kindr::minimal::QuatTransformationTemplate<double> SE3;
 template<>
@@ -69,6 +71,19 @@ struct DefaultChart<SE3> {
 
 } //namespace gtsam
 
+// Expression to calculate relative measurement between 2 SE3 values
+// \todo AG move this to trajectory maintainer at some point (this replaces relative pose factor functionality)
+ValueType relativeMeasurementExpression(const ValueType& interp1,
+                                        const ValueType& interp2,
+                                        boost::optional<Eigen::Matrix<double,6,6>&>H1=boost::none,
+                                        boost::optional<Eigen::Matrix<double,6,6>&>H2=boost::none) {
+  if (H1) { *H1 = Eigen::Matrix<double,6,6>::Identity(); }
+  if (H2) { *H2 = - Eigen::Matrix<double,6,6>::Identity(); }
+
+  return ValueType(gtsam::DefaultChart<ValueType>::local(interp1, interp2));
+}
+
+// test for correct keys and evaluation function in Slerp SE3 curves
 TEST(CurvesTestSuite, testSlerpSE3ExpressionKeysAndEvaluation) {
 
   SlerpSE3Curve curve;
@@ -84,7 +99,6 @@ TEST(CurvesTestSuite, testSlerpSE3ExpressionKeysAndEvaluation) {
   values.push_back(poseB);
 
   curve.fitCurve(times, values);
-  curve.print("slerp curve");
 
   KeyCoefficientTime *rval0, *rval1;
   curve.getCoefficientsAt(evalTime, &rval0, &rval1);
@@ -121,6 +135,7 @@ TEST(CurvesTestSuite, testSlerpSE3ExpressionKeysAndEvaluation) {
   ASSERT_NEAR(resultRot(3),0.0,1e-6);
 }
 
+// test basic gtsam interface of Slerp SE3 curves
 TEST(CurvesTestSuite, testSlerpSE3ExpressionGTSAMoptimization) {
 
   SlerpSE3Curve curve;
@@ -194,6 +209,44 @@ TEST(CurvesTestSuite, testSlerpSE3ExpressionGTSAMoptimization) {
   // Optimize
   gtsam::Values result = gtsam::LevenbergMarquardtOptimizer(graph, initials).optimize();
   ASSERT_TRUE(expected.equals(result,1e-6));
+}
+
+// test for dynamic handling of duplicate keys in gtsam::ExpressionFactor
+TEST(CurvesTestSuite, testSlerpSE3ExpressionDynamicKeys){
+  SlerpSE3Curve curve;
+
+  // Populate coefficients
+  const double t[] = {0, 45, 90, 135};
+  std::vector<Time> times(t,t+4);
+  std::vector<ValueType> coefficients;
+
+  // create a set of coefficients
+  coefficients.push_back(ValueType(SO3(SO3::Vector4(1,0,0,0)),SE3::Position(0,0,0)));
+  coefficients.push_back(ValueType(SO3(SO3::Vector4(0.9238795325112867,0,-0.3826834323650897,0)),SE3::Position(4.5,4.5,4.5)));
+  coefficients.push_back(ValueType(SO3(SO3::Vector4(0.7071067811865476,0,-0.7071067811865476,0)),SE3::Position(9,9,9)));
+  coefficients.push_back(ValueType(SO3(SO3::Vector4(0.38268343236508984,0,-0.9238795325112866,0)),SE3::Position(13.5,13.5,13.5)));
+
+  // Populate measurements
+  const double tmeas[] = {10, 30, 20};
+  std::vector<Time> measTimes(tmeas,tmeas+3);
+  const double durations[] = {30, 30, 100};
+  std::vector<ValueType> measurements;
+  measurements.push_back(ValueType(SO3(SO3::Vector4(0.9659258262890683,0,-0.25881904510252074,0)),SE3::Position(3,3,3)));
+  measurements.push_back(ValueType(SO3(SO3::Vector4(0.9659258262890683,0,-0.25881904510252074,0)),SE3::Position(3,3,3)));
+  measurements.push_back(ValueType(SO3(SO3::Vector4(0.6427876096865394,0,-0.766044443118978,0)),SE3::Position(10,10,10)));
+
+  // Fit a curve
+  curve.fitCurve(times, coefficients);
+  noiseModel::Diagonal::shared_ptr measNoiseModel = noiseModel::Diagonal::Sigmas((gtsam::Vector(6) << 0.1, 0.1, 0.1, 0.1, 0.1, 0.1));
+
+  // create ExpressionFactor which represents relative pose relation (former Relative pose factor)
+  for (int i=0; i<3; ++i) {
+    Expression<ChartValue<ValueType> > relative (relativeMeasurementExpression, curve.getEvalExpression(tmeas[i]), curve.getEvalExpression(tmeas[i]+durations[i]));
+    ExpressionFactor<ChartValue<ValueType> > factor(measNoiseModel,ChartValue<ValueType>(measurements[i]), relative);
+    const FastVector<Key> keys = factor.keys();
+    // check that the ExpressionFactor dynamically handles duplicate keys
+    ASSERT_EQ(factor.keys().size(),i+2);
+  }
 }
 
 TEST(CurvesTestSuite, testSlerpSE3ExpressionJacobians){
