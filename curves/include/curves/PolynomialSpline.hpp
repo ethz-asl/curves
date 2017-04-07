df45955d9a81c56e67496d17563c036dab39f297
dbellicoso <bellicosodario@gmail.com>
2017-04-07 19:27:11 +0200

    Added comments. Removed unused variable.

--- curves/include/curves/PolynomialSpline.hpp
+++ curves/include/curves/PolynomialSpline.hpp
@@ -15,6 +15,14 @@
 
 namespace curves {
 
+/*
+ * This class is the implementation of a scalar polinomial spline s(t) function of a scalar t.
+ * The spline is define as
+ *    s(t) = an*t^n + ... + a1*t + a0 = sum(ai*t^i)
+ *
+ *  The spline coefficients are stored in a standard container as
+ *    alpha = [an ... a1 a0]
+ */
 template <int splineOrder_>
 class PolynomialSpline {
  public:
@@ -28,7 +36,6 @@ class PolynomialSpline {
   using EigenCoefficientVectorType = Eigen::Matrix<double, coefficientCount, 1>;
 
   PolynomialSpline() :
-    currentTime_(0.0),
     duration_(0.0),
     didEvaluateCoeffs_(false),
     coefficients_()
@@ -40,21 +47,31 @@ class PolynomialSpline {
 
   }
 
+  PolynomialSpline(PolynomialSpline &&) = default;
+  PolynomialSpline& operator=(PolynomialSpline &&) = default;
+
+  PolynomialSpline(const PolynomialSpline&) = default;
+  PolynomialSpline& operator=(const PolynomialSpline&) = default;
+
+  //! Get the coefficients of the spline.
   const SplineCoefficients& getCoefficients() const {
     return coefficients_;
   }
 
+  //! Compute the coefficients of the spline.
   bool computeCoefficients(const SplineOptions& options) {
     SplineImplementation::compute(options, coefficients_);
     duration_ = options.tf_;
     return true;
   }
 
+  //! Set the coefficients and the duration of the spline.
   void setCoefficientsAndDuration(const SplineCoefficients& coefficients, double duration) {
     coefficients_ = coefficients;
     duration_ = duration;
   }
 
+  //! Set the coefficients and the duration of the spline.
   void setCoefficientsAndDuration(const EigenCoefficientVectorType& coefficients, double duration) {
     for (unsigned int k=0; k<coefficientCount; k++) {
       coefficients_[k] = coefficients(k);
@@ -62,65 +79,60 @@ class PolynomialSpline {
     duration_ = duration;
   }
 
+  //! Get the spline evaluated at time tk.
   constexpr double getPositionAtTime(double tk) const {
     return std::inner_product(coefficients_.begin(), coefficients_.end(),
                               SplineImplementation::tau(std::max(0.0, std::min(tk, duration_))).begin(), 0.0);
   }
 
+  //! Get the first derivative of the spline evaluated at time tk.
   constexpr double getVelocityAtTime(double tk) const {
     return std::inner_product(coefficients_.begin(), coefficients_.end(),
                               SplineImplementation::dtau(std::max(0.0, std::min(tk, duration_))).begin(), 0.0);
   }
 
+  //! Get the second derivative of the spline evaluated at time tk.
   constexpr double getAccelerationAtTime(double tk) const {
     return std::inner_product(coefficients_.begin(), coefficients_.end(),
                               SplineImplementation::ddtau(std::max(0.0, std::min(tk, duration_))).begin(), 0.0);
   }
 
+  //! Get the time vector tau evaluated at time tk.
   static inline void getTimeVector(Eigen::Ref<EigenTimeVectorType> timeVec, double tk) {
     timeVec = Eigen::Map<EigenTimeVectorType>(SplineImplementation::tau(tk).data());
   }
 
+  //! Get the first derivative of the time vector tau evaluated at time tk.
   static inline void getdTimeVector(Eigen::Ref<EigenTimeVectorType> dtimeVec, double tk) {
     dtimeVec = Eigen::Map<EigenTimeVectorType>(SplineImplementation::dtau(tk).data());
   }
 
+  //! Get the second derivative of the time vector tau evaluated at time tk.
   static inline void getddTimeVector(Eigen::Ref<EigenTimeVectorType> ddtimeVec, double tk) {
     ddtimeVec = Eigen::Map<EigenTimeVectorType>(SplineImplementation::ddtau(tk).data());
   }
 
+  //! Get the time vector tau evaluated at zero.
   static inline void getTimeVectorAtZero(Eigen::Ref<EigenTimeVectorType> timeVec) {
-    timeVec = Eigen::Map<EigenTimeVectorType>(SplineImplementation::tauZero.data());
+    timeVec = Eigen::Map<const EigenTimeVectorType>((SplineImplementation::tauZero).data());
   }
 
+  //! Get the first derivative of the time vector tau evaluated at zero.
   static inline void getdTimeVectorAtZero(Eigen::Ref<EigenTimeVectorType> dtimeVec) {
-    dtimeVec = Eigen::Map<EigenTimeVectorType>(SplineImplementation::dtauZero.data());
+    dtimeVec = Eigen::Map<const EigenTimeVectorType>((SplineImplementation::dtauZero).data());
   }
 
+  //! Get the second derivative of the time vector tau evaluated at zero.
   static inline void getddTimeVectorAtZero(Eigen::Ref<EigenTimeVectorType> ddtimeVec) {
-    ddtimeVec = Eigen::Map<EigenTimeVectorType>(SplineImplementation::ddtauZero.data());
+    ddtimeVec = Eigen::Map<const EigenTimeVectorType>((SplineImplementation::ddtauZero).data());
   }
 
-  void advanceTime(double dt) {
-    currentTime_ += dt;
-  }
-
-  void resetTime() {
-    currentTime_ = 0.0;
-  }
-
-  double getTime() const {
-    return currentTime_;
-  }
-
+  //! Get the duration of the spline in seconds.
   double getSplineDuration() const {
     return duration_;
   }
 
  protected:
-  //! A helper counter which used to get the state of the spline.
-  double currentTime_;
-
   //! The duration of the spline in seconds.
   double duration_;
 
